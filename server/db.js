b0267cb6ea83a539d24095de9f55d3aab58d49fe
joaomiguelcode <joaomiguelcode@users.noreply.github.com>
2026-08-28 14:56:16 -0300

    chore: update database connection config

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || '82.40.22.100',
   port: parseInt(process.env.DB_PORT || '3306'),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  user: process.env.DB_USER || 'Orbit',
+  password: process.env.DB_PASSWORD || '10203040',
   database: process.env.DB_NAME || 'orbit_db',
   waitForConnections: true,
   connectionLimit: 15,
